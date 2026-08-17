aec4b6e0d44bdc73dad0191f5c6b6f22bf60711d
KUMAR SHANTANU <shantanu22505@gmail.com>
2026-08-17 21:49:40 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n = s.size();
+        for(char c: s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }
+            else{
+                if(st.empty()) return false;
+                char top = st.top();
+                st.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')){
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
